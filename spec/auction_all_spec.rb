8998f38c3fbf37e7b5f4e6f859393e10c5925768
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-08 13:28:49 -0700

    Add successful readable attributes test for the attendee class

--- spec/auction_all_spec.rb
+++ spec/auction_all_spec.rb
@@ -25,5 +25,12 @@ RSpec.describe Attendee do
             attendee = Attendee.new(name: 'Megan', budget: '$50')
             expect(attendee).to be_instance_of(Attendee)
         end
+
+        it "has readable attributes" do
+            attendee = Attendee.new(name: 'Megan', budget: '$50')
+
+            expect(attendee.name).to eq('Megan')
+            expect(attendee.budget).to eq('$50')
+        end
     end
 end
