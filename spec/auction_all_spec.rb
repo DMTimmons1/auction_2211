09fa9aa74886e6ba6c48f5df6b6198164682adcd
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-08 13:27:05 -0700

    Add successful readable attributes test for the item class

--- spec/auction_all_spec.rb
+++ spec/auction_all_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe Item do
             item1 = Item.new('Chalkware Piggy Bank')
             expect(item1).to be_instance_of(Item)
         end
+
+        it "has readable attributes" do
+            item1 = Item.new('Chalkware Piggy Bank')
+            item2 = Item.new('Bamboo Picture Frame')
+
+            expect(item1.name).to eq('Chalkware Piggy Bank')
+            expect(item2.name).to eq('Bamboo Picture Frame')
+        end
     end
 end
